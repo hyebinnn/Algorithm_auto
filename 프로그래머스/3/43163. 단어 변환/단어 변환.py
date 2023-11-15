5aa01aa7584f26f7d2e8a202722dd1403e06bc18
hyebinnn <76420055+hyebinnn@users.noreply.github.com>
2023-11-15 21:04:10 +0900

    [level 3] Title: 단어 변환, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,30 +1,57 @@
-from collections import deque
+# from collections import deque
 
 def solution(begin, target, words):
-    queue = deque()
-    queue.append((begin, 0))
-    d = dict()
-    visited = dict()
-    for x in words:
-        d[x] = int(1e6)
-        visited[x] = 0
+    res = []
+    visited = []
     if target not in words:
         return 0
-    def getSimilarWord(current, cnt):
+    def dfs(current, cnt):
+        visited.append(current)
         for w in words:
             diff = 0
             for j in range(len(current)):
                 if w[j] != current[j]:
                     diff += 1
-            if diff == 1 and not visited[w]:
-                d[w] = min(d[w], cnt+1)
-                queue.append((w, cnt+1))
+            if diff == 1 and w not in visited:
+                if w == target:
+                    res.append(cnt+1)
+                    return
+                dfs(w, cnt+1)
                 
-    cnt = 0
-    while queue:
-        word, cnt = queue.popleft()
-        visited[word] = 1
-        getSimilarWord(word, cnt)
+    
+    dfs(begin, 0)
+    
+    return min(res)
+                
+        
+    
+    
+#### BFS
+# =========================
+#     queue = deque()
+#     queue.append((begin, 0))
+#     d = dict()
+#     visited = dict()
+#     for x in words:
+#         d[x] = int(1e6)
+#         visited[x] = 0
+#     if target not in words:
+#         return 0
+#     def getSimilarWord(current, cnt):
+#         for w in words:
+#             diff = 0
+#             for j in range(len(current)):
+#                 if w[j] != current[j]:
+#                     diff += 1
+#             if diff == 1 and not visited[w]:
+#                 d[w] = min(d[w], cnt+1)
+#                 queue.append((w, cnt+1))
+                
+#     cnt = 0
+#     while queue:
+#         word, cnt = queue.popleft()
+#         visited[word] = 1
+#         getSimilarWord(word, cnt)
                           
-    return d[target]
+#     return d[target]
         
