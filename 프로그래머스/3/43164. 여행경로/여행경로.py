898adf81f9d3820b3237ca921202149e46f7eb03
hyebinnn <76420055+hyebinnn@users.noreply.github.com>
2023-11-21 19:52:09 +0900

    [level 3] Title: 여행경로, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -8,14 +8,13 @@ def solution(tickets):
                 visited[i] = 1
                 getRoute(tickets[i][1])
                 visited[i] = 0
-                temp.pop()
-                # print(visited, end)
+                temp.pop()              # for 백트래킹
                 
             elif tickets[i][0] != end and i == len(tickets) - 1 and 0 not in visited:
                 temp.append(end)
                 temp2 = temp.copy()
                 result.append(temp2)
-                temp.pop()
+                temp.pop()                  # 모든 항공권 사용 후 마지막 도착 도시 제거 (for 백트래킹) 
                     
     
     for i in range(len(tickets)):
@@ -24,9 +23,6 @@ def solution(tickets):
             temp = ["ICN"]
             visited[i] = 1
             getRoute(tickets[i][1])
-            # result.append(temp)
             
-    # print(result)
     result.sort()
-    # print(result)
     return result[0]
