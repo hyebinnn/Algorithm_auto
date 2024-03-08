45cb658305dac2c2f212f46f47a133e0bd54514a
hyebinnn <76420055+hyebinnn@users.noreply.github.com>
2024-03-09 03:21:04 +0900

    [Silver II] Title: 유기농 배추, Time: 308 ms, Memory: 31500 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,3 +1,5 @@
+# # 유기농 배추
+# dfs
 import sys
 sys.setrecursionlimit(10000)
 
@@ -7,20 +9,13 @@ dy = [0, 1, 0, -1]
 T = int(input())
 for _ in range(T):
     m, n, k = map(int, input().split())
-    garden = [[0]*m for _ in range(n)]    
-    sx, sy = map(int, input().split())
-    garden[sy][sx] = 1
-    for _ in range(k-1):
+    garden = [[0]*m for _ in range(n)]
+    location = set()   
+    for _ in range(k):
         x, y = map(int, input().split())
         garden[y][x] = 1
-        
-    def getIdxOne(board):
-        for i in range(len(board)):
-            if 1 in board[i]:
-                j = board[i].index(1)
-                return (j, i)
-        return -1
-
+        location.add((x, y))
+       
     count = 0
     
     def dfs(idx):
@@ -31,13 +26,9 @@ for _ in range(T):
             if 0 <= nx < m and 0 <= ny < n and garden[ny][nx] == 1:
                 dfs((nx, ny))
 
-    dfs((sx, sy))
-    count += 1   
-    res = getIdxOne(garden)
-
-    while res != -1:
-        dfs(res)
-        count += 1
-        res = getIdxOne(garden)
+    for x, y in location:
+        if garden[y][x]:
+            dfs((x, y))
+            count += 1
     
     print(count)        
