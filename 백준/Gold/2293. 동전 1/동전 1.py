c75229d3ae6035e1b756f127ff666c80b4f0d366
hyebinnn <76420055+hyebinnn@users.noreply.github.com>
2024-03-21 05:52:26 +0900

    [Gold V] Title: 동전 1, Time: 220 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.py
@@ -0,0 +1,12 @@
+n, k = map(int, input().split())
+coin = [int(input()) for _ in range(n)]
+dp = [0] * (k+1)
+dp[0] = 1
+    
+for i in coin:
+    for j in range(i, k+1):
+        if j >= i:
+            dp[j] += dp[j-i]
+            
+print(dp[k])
+
