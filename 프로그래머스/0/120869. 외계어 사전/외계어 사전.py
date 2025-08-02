427162e48f0094952547a5c05b4faf6b4d419b3c
hyebinnn <76420055+hyebinnn@users.noreply.github.com>
2025-08-02 13:24:37 +0900

    [level 0] Title: 외계어 사전, Time: 0.01 ms, Memory: 9.1 MB -BaekjoonHub

--- 프로그래머스/0/120869. 외계어 사전/외계어 사전.py
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.py
@@ -1,15 +1,5 @@
-def rotation(spell, d):
-    for s in spell:
-        if s not in d:
-            return -1
-    return 1
-        
-
 def solution(spell, dic):
-    for s in spell:
-        for d in dic:
-            if s in d:
-                if rotation(spell, d) == 1:
-                    return 1
-        return 2
-    
+    for d in dic:
+        if sorted(d) == sorted(spell):
+            return 1
+    return 2
