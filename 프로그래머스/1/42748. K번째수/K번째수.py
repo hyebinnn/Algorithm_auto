dfe55841974c81144a1529cb49cc504675162c6f
hyebinnn <76420055+hyebinnn@users.noreply.github.com>
2026-07-24 10:39:09 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 11.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -0,0 +1,9 @@
+def solution(array, commands):
+    answer = []
+    
+    for command in commands:
+        i,j,k = command
+        arr = sorted(array[i-1:j])
+        answer.append(arr[k-1])
+    
+    return answer
